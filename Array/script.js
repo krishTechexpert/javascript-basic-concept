2a486591b4a14264fe57fb77d9eccac25a32464c
krish <krishcode9@gmail.com>
2024-09-25 17:55:23 +0530

    Bankist app has completed

--- Array/script.js
+++ Array/script.js
@@ -397,3 +397,91 @@ const allAccounts2 = JSON.parse(JSON.stringify(accounts)) // copy deep level
 const allMovementsBalanceDeepLevel = allAccounts2.flatMap(acc => acc.movements).reduce((acc,cur) => acc + cur,0 )
 console.log(allMovementsBalanceDeepLevel) // 17840
 
+
+// Array fill
+const x= [1,2,3,4,5,6,7];
+const x1=new Array(7) // [empty*7]
+
+const x11=x1.fill(20,2,4) // start position=2, end position=4 end part exclude same work as slice
+console.log(x11) // [empty *2,20,20,empty*3]
+
+// Array.from() is also iteratable method
+// The Array.from() static method creates a new, shallow-copied Array instance from an iterable or array-like object
+const y = Array.from({length:7},() => 1) // [1,1,1,1,1,1,1]
+
+const z = Array.from({length:7},(cur,i) => i+1) // [1,2,3,4,5,6,7] work same as map
+//const z = Array.from({length:7},(_,i) => i+1) // here _represent that no need of current element
+
+console.log(Array.from('foo'));
+// Expected output: Array ["f", "o", "o"]
+
+console.log(Array.from([1, 2, 3], (x) => x + x));
+// Expected output: Array [2, 4, 6]
+
+//Array from a NodeList
+labelBalance.addEventListener('click',function(){
+const movementsUI =   Array.from(document.querySelectorAll('.movements__value'),(e1) => Number(e1.textContent.replace('€','')) )
+console.log(movementsUI)
+})
+
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Julia and Kate are still studying dogs, and this time they are studying if dogs are eating too much or too little.
+Eating too much means the dog's current food portion is larger than the recommended portion, and eating too little is the opposite.
+Eating an okay amount means the dog's current food portion is within a range 10% above and 10% below the recommended portion (see hint).
+
+1. Loop over the array containing dog objects, and for each dog, calculate the recommended food portion and add it to the object as a new property. Do NOT create a new array, simply loop over the array. Forumla: recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, and the weight needs to be in kg)
+2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple owners, so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose) 🤓
+3. Create an array containing all owners of dogs who eat too much ('ownersEatTooMuch') and an array with all owners of dogs who eat too little ('ownersEatTooLittle').
+4. Log a string to the console for each array created in 3., like this: "Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
+5. Log to the console whether there is any dog eating EXACTLY the amount of food that is recommended (just true or false)
+6. Log to the console whether there is any dog eating an OKAY amount of food (just true or false)
+7. Create an array containing the dogs that are eating an OKAY amount of food (try to reuse the condition used in 6.)
+8. Create a shallow copy of the dogs array and sort it by recommended food portion in an ascending order (keep in mind that the portions are inside the array's objects)
+
+HINT 1: Use many different tools to solve these challenges, you can use the summary lecture to choose between them 😉
+HINT 2: Being within a range 10% above and below the recommended portion means: current > (recommended * 0.90) && current < (recommended * 1.10). Basically, the current portion should be between 90% and 110% of the recommended portion.
+
+GOOD LUCK 😀
+*/
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+//1
+dogs.forEach(dog => dog.recommendedFood = Math.trunc(dog.weight ** 0.75 * 28))
+console.log(dogs) //284,133,191,376
+//2
+const sarahDog = dogs.find(dog => dog.owners.includes('Sarah'))
+console.log(sarahDog)
+console.log(`Sarah 's dog eating ${sarahDog.curFood > sarahDog.recommendedFood ? 'too much' : 'too little'} `)
+//3
+const ownersEatTooMuch = dogs.filter(dog => dog.curFood>dog.recommendedFood ).flatMap(dog => dog.owners);
+console.log(ownersEatTooMuch)
+const ownersEatTooLittle = dogs.filter(dog => dog.curFood<dog.recommendedFood ).flatMap(dog => dog.owners);
+console.log(ownersEatTooLittle)
+
+//4
+console.log(`${ownersEatTooMuch.join(' and ')}'s dogs eat too much!`)
+console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eat too little!`)
+//5
+console.log(dogs.some(dog => dog.curFood === dog.recFood));
+// 6.
+// current > (recommended * 0.90) && current < (recommended * 1.10)
+const checkEatingOkay = dog =>
+  dog.curFood > dog.recFood * 0.9 && dog.curFood < dog.recFood * 1.1;
+console.log(dogs.some(checkEatingOkay));
+
+// 7.
+console.log(dogs.filter(checkEatingOkay));
+
+// 8.
+// sort it by recommended food portion in an ascending order [1,2,3]
+const dogsSorted = dogs.slice().sort((a, b) => a.recFood - b.recFood);
+console.log(dogsSorted);
