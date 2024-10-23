ac0fa04ec2a8eb2a1ec70a326d154821ccf5908d
krish <krishcode9@gmail.com>
2024-10-24 00:45:38 +0530

    bookmark done

--- 18-forkify/src/js/views/bookmarksView.js
+++ 18-forkify/src/js/views/bookmarksView.js
@@ -12,7 +12,20 @@ class BookmarksView extends View {
   }
 
   _generateMarkup(){
-      return this._data.map(bookmark => previewView.render(bookmark,false)).join(''); // some recursion technique used ho rehi hai for code html reused inside _generateMarkup in PreviewView for previewView.render()..plz observe code twice
+      return this._data.map(bookmark => previewView.render(bookmark,false)).join(''); 
+
+  /*Flow Summary: explain above line of code
+  1.) _generateMarkup() is called.
+  2.) this._data.map() loops through each bookmark in the array.
+  3.) For each bookmark, previewView.render(bookmark, false) is called:
+            Inside render(), this._data in previewView is set to the bookmark.
+            previewView._generateMarkup() is called to generate the HTML for that bookmark.
+            The HTML string is returned.
+  4.) map() returns an array of HTML strings, one for each bookmark.
+  5.) join('') concatenates the array of strings into one large HTML string.
+  6.) The final HTML string is returned by _generateMarkup().
+  This ensures that when previewView._generateMarkup() is called, it has access to the current bookmark’s data (like id, title, image).*/
+
   }
 
 } 
